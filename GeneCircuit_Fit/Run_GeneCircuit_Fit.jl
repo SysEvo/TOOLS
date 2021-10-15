6e9334fcbb74b4c0fa42c82d5633d357a29c9d2e
Mariana Gómez-Schiavon, PhD <mgschiavon@users.noreply.github.com>
2021-10-15 15:38:54 -0700

    Formatting

--- GeneCircuit_Fit/Run_GeneCircuit_Fit.jl
+++ GeneCircuit_Fit/Run_GeneCircuit_Fit.jl
@@ -1,6 +1,6 @@
 ## Running in julia terminal
 	cd("\\TOOLS\\GeneCircuit_Fit\\")	# Go to files location
 	using Pkg; Pkg.activate(".");		# Activate local environment (requiere '.toml' files)
-	iARG = (mm = "ToyEx1",				# Label for model file (e.g. 'A' for 'Library\Md_A.jl' file)
-			ex = "Ex01");				# Label for parameters file (e.g. 'Ex01' for 'InputFiles\ARGS_A_Par_Ex01.jl' file)
+	iARG = (mm = "ToyEx1",			# Label for model file (e.g. 'A' for 'Library\Md_A.jl' file)
+			ex = "Ex01");		# Label for parameters file (e.g. 'Ex01' for 'InputFiles\ARGS_A_Par_Ex01.jl' file)
 	include("GeneCircuit_Fit.jl")		# Run fiting code
